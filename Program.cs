9d2264be4aa386877f8cf5d59255e34735ec5e21
Cleberson Pinheiro <clebersondesign2012@gmail.com>
2023-02-13 12:04:29 -0300

    Updated App teste

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MyApp
+{
+
+    static void Main(StringSplitOptions[] args)
+    {
+        Soma(8,2);
+        Console.ReadLine();
+    }
+
+    public static int Soma(int num1, int num2);
+    {
+        int result = num1 + num2;
+        Console.writeLine("O resultado é: " + result);
+    }
+}
